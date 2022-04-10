89f07cf81ed73844599829267ccfeb12d851f038
João Renato <joaorenatovaz@gmail.com>
2022-04-10 20:52:43 -0300

    Modo 1x1 pronto

--- JS/game.js
+++ JS/game.js
@@ -1,48 +1,173 @@
-import { man} from "./options.js";
-
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
+const panel = document.querySelector(".panel");
+const player = document.querySelector(".player");
+const panel2 = document.querySelector(".panel2");
 
+const selectBox = document.querySelector("select-box");
+const selectBtnX = document.querySelector(".playerX");
+const selectBtnO = document.querySelector(".playerO");
+const selectBtnAgain = document.querySelector("[button]");
+const selectBtnAgainTie = document.querySelector("[button-tie]");
+
+const winnermessage = document.querySelector(".winning-message");
+const timessage = document.querySelector(".tie-message");
+const message = document.querySelector("[data-winning-message-text]")
+const confet = document.querySelector("#my-canvas")
+
+let isCircleTurn;
+const winnigCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+var confettiSettings = {
+    target: 'my-canvas'
+};
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
+
+const removeClass = () => {
+    if(isCircleTurn == "x"){
+        board.classList.remove("circle");
+    }else{
+        board.classList.remove("x");
+    }
+
+}
+
+window.onload = () => {
+    selectBtnX.onclick = () => {
+        panel2.classList.add("hide");
+        panel.classList.add("show");
+        player.classList.add("show");
+        isCircleTurn = false;
+        board.classList.add("x");
+
+    }
+    selectBtnO.onclick = () => {
+        panel2.classList.add("hide");
+        panel.classList.add("show");
+        player.classList.add("show");
+        isCircleTurn = true;
+        board.classList.add("circle");
+    }
+}
 
 
 const startGame = () => {
 
+    
+
+    for (const cell of cellElements) {
+        cell.classList.remove("circle");
+        cell.classList.remove("x");
+        board.classList.remove("x");
+        board.classList.remove("circle");
+        cell.removeEventListener("click", handClick);
+
+        cell.addEventListener("click", handClick, {
+            once: true
+        });
+    }
+    
+
+    panel2.classList.remove("hide");
+    panel.classList.remove("show");
+    player.classList.remove("show");
+    winnermessage.classList.remove("active");
+    timessage.classList.remove("active");
+    player.classList.remove("hide");
+    confet.classList.remove("active");
+    
+    isCircleTurn;
+
+};
+
+const endGame = (isDraw) => {
+    if (isDraw) {
+
+        timessage.classList.add("active");
+        player.classList.add("hide");
+
+    } else {
+        winnermessage.classList.add("active");
+        player.classList.add("hide");
+        message.innerText = isCircleTurn ? "Circulo Venceu" : "X venceu";
+        confet.classList.add("active");
+    }
 };
 
 
+
+const checkforwin = (currentPlayer) => {
+    return winnigCombination.some((combination) => {
+        return combination.every((index) => {
+            return cellElements[index].classList.contains(currentPlayer);
+        });
+    });
+};
+
+const checkForDraw = () => {
+    return [...cellElements].every((cell) => {
+      return cell.classList.contains("x") || cell.classList.contains("circle");
+    });
+  };
 const placeMark = (cell, classtoAdd) => {
     cell.classList.add(classtoAdd);
 };
 
-const swapTurns = () => {
-    man = !man;
-
+const setBoardHoverClass = () => {
     board.classList.remove('circle');
     board.classList.remove('x');
 
-    if (man) {
+    if (isCircleTurn) {
         board.classList.add('circle');
 
     } else {
         board.classList.add('x');
 
     }
+}
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    setBoardHoverClass();
 };
+
 
 const handClick = (e) => {
     // Colocar o X ou O
     const cell = e.target;
-    const classtoAdd = man ? "circle" : "x";
-
+    const classtoAdd = isCircleTurn ? "circle" : "x";
+    
     placeMark(cell, classtoAdd);
     // Verificar Vitoria
+    const isWin = checkforwin(classtoAdd);
     //Verificar Empate
-    //Mudar Simbolo
-    swapTurns();
+    const isDraw = checkForDraw();
+    if (isWin) {
+        endGame(false)
+
+    }else if (isDraw){
+        endGame(true)
+    }else{
+        //Mudar Simbolo
+        swapTurns();
+    }
+    
+
+    
+    
 };
 
-for (const cell of cellElements) {
-    cell.addEventListener("click", handClick, {
-        once: true
-    });
-}
+startGame();
+selectBtnAgain.addEventListener("click", startGame);
+selectBtnAgainTie.addEventListener("click", startGame);
--- JS/options.js
+++ JS/options.js
@@ -1,23 +1,15 @@
 // SELECIONAR ELEMENTOS
+
 const options = document.querySelector(".right-options");
-
-
 
 // SELECIONAR BUTTONS
 const computerBtn = options.querySelector(".computer");
 const friendBtn = options.querySelector(".friend");
-const xBtn = options.querySelector(".x");
-const oBtn = options.querySelector(".o");
 const playBtn = options.querySelector(".play");
 
 // VARIAVES PARA GUARDAR INFORMAÇÕES DO USUARIO
 let OPPONENT;
-export var man;
-export var computer;
-export var friend;
 
-
-// ADICONANDO EVENTO EM CADA BUTTON
 computerBtn.addEventListener("click", function () {
     OPPONENT = "computer";
     if (OPPONENT == "computer") {
@@ -40,35 +32,6 @@ friendBtn.addEventListener("click", function () {
     switchActive(computerBtn, friendBtn);
 });
 
-xBtn.addEventListener("click", function () {
-    man = "x";
-    computer = "circle";
-    friend = "cirlce";
-    if (man == "x") {
-        xBtn.style.backgroundColor = "#00ff88";
-        oBtn.style.backgroundColor = "#514869";
-        
-        return;
-    }
-    
-
-    switchActive(oBtn, xBtn);
-});
-
-oBtn.addEventListener("click", function () {
-    man = "circle";
-    computer = "x";
-    friend = "x";
-    if (man == "circle") {
-        xBtn.style.backgroundColor = "#514869";
-        oBtn.style.backgroundColor = "#00ff88";
-
-        return;
-    }
-
-    switchActive(xBtn, oBtn);
-});
-
 
 playBtn.addEventListener("click", function () {
     // CHECK SE O USER ESCOLHEU UM OPONENTE
@@ -79,30 +42,17 @@ playBtn.addEventListener("click", function () {
     }
 
     // CHECK SE O USER ESCOLHEU UM SIMBOLO
-    if (!man) {
-        xBtn.style.backgroundColor = "#f00";
-        oBtn.style.backgroundColor = "#f00";
-
-        return;
-    }
 
     // RUN THE GAME
     if (OPPONENT == "computer") {
         window.location.href = "AI.html";
-        
+
     }
     if (OPPONENT == "friend") {
         window.location.href = "friend.html";
-        
-        
+
+
     }
 
 });
 
-export{computerBtn,friendBtn,xBtn,oBtn,playBtn};
-
-// SWITCH ACTIVE CLASS BETWEEN TWO ELEMENTS
-function switchActive(off, on) {
-    off.classList.remove("active");
-    on.classList.add("active");
-}
